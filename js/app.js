bc00da150637459222ab6973b73893adeffb9230
Ivana Todorovic <todorovicivana.it@gmail.com>
2023-04-15 16:14:41 +0200

    feat: apply prettier and addd src as root directory

--- js/app.js
+++
@@ -1,7 +0,0 @@
-const tel = document.getElementById('tel');
-tel.addEventListener('mouseover', function () {
-    tel.style.fontSize = '60px';
-})
-tel.addEventListener('mouseout', function () {
-    tel.style.fontSize = '40px';
-})
